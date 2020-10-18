88972344c2783ed69ed412187d95bea07e217a26
Junioresc <junioresc1092@gmail.com>
2020-10-18 15:28:43 -0400

    added the header and footer components

---
+++ src/components/Footer/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Footer() {
+
+    return (
+      <section className="page-section contact" id="contact">
+        <h2>Contact Me</h2>
+        <div>
+          <address>
+            <a href="tel:+5615778575">(561)577-8575</a>
+            <a href="mailto:junioresc1092@gmail.com">junioresc1092@gmail.com</a>
+            <a href="https://github.com/junioresc">GitHub</a>
+            <a href="https://www.linkedin.com/in/junior-escobar-87a8541a7/">
+              LinkedIn
+            </a>
+          </address>
+        </div>
+      </section>
+    );
+}
+
+export default Footer;
---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Nav from '../Nav';
+
+function Header() {
+    
+    return (
+        <header>
+            <h1>
+                <a href="/">Junior Escobar</a>
+            </h1>
+        </header>
+    );
+}
+
+export default Header;
