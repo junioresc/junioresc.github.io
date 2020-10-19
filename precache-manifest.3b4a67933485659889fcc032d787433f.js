8004863b666b0e88c728e94c5e39d4b0ffba12e3
Junioresc <junioresc1092@gmail.com>
2020-10-18 20:50:17 -0400

    Updates

---
+++ precache-manifest.3b4a67933485659889fcc032d787433f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09ed978438183fe281095f8bf848ae31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98294785b20d9e7baf4f",
+    "url": "/static/css/main.879db04a.chunk.css"
+  },
+  {
+    "revision": "0a948a1205e5f0d48c7b",
+    "url": "/static/js/2.48a2b034.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.48a2b034.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98294785b20d9e7baf4f",
+    "url": "/static/js/main.51ab6950.chunk.js"
+  },
+  {
+    "revision": "dbcdf457667c3c25dc8a",
+    "url": "/static/js/runtime-main.532fc730.js"
+  },
+  {
+    "revision": "3ff478989213bffa32e12022026db815",
+    "url": "/static/media/dc-adopt.3ff47898.PNG"
+  },
+  {
+    "revision": "de1ea457f63e5fcd7632f246668fb5ec",
+    "url": "/static/media/hero-bg.de1ea457.jpg"
+  },
+  {
+    "revision": "e84f862bd911e784e1ca09914fe09cb2",
+    "url": "/static/media/horiseon-nav-with-hero.e84f862b.jpg"
+  },
+  {
+    "revision": "13430b95b4780c260d7bcc337ec2ea55",
+    "url": "/static/media/junior.13430b95.JPG"
+  },
+  {
+    "revision": "c0fa71c6b462f43f03c0df3104cc05ad",
+    "url": "/static/media/nomadica.c0fa71c6.jpg"
+  },
+  {
+    "revision": "4288d574d2cbeaa240231b79356968df",
+    "url": "/static/media/password-gen-site.4288d574.PNG"
+  },
+  {
+    "revision": "180aef31a05a8a6bde5fa90634c63afa",
+    "url": "/static/media/pizza-hunt.180aef31.PNG"
+  },
+  {
+    "revision": "430894fa84a999c4ef7b42c537d7e3bb",
+    "url": "/static/media/portfolio.430894fa.jpg"
+  },
+  {
+    "revision": "36c81afe124efdaef85c92176c73fbca",
+    "url": "/static/media/run-buddy.36c81afe.jpg"
+  }
+]);
